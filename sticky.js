8e7c0ce132ae8bdf66170d6a42c18392003ee22f
Mr. Johnson <isrealism2003@yahoo.com>
2016-05-13 14:52:54 +0100

    Update sticky.js
    The Sticky Module re-implemented to support simultaneous simulation of different elements

--- sticky.js
+++ sticky.js
@@ -1,181 +1,185 @@
-/**
-@Description 
- A simple javascript  Function that monitor and keep dom element always visible.
 
- @Param {element} its take an element or element id
 
- @Author Obaro I. Johnson
-
-*/
-
-var Sticky =(function(elementOrId)
+var Sticky =(function(elementid)
 {
-	this.initTop=0;
-	if(elementOrId instanceof HTMLElement){
-		this.element= elementOrId;	
-	}else if(typeof elementOrId =="string"){
-		this.element = document.getElementById(elementOrId);
-	}
-	if(!this.element) throw new Error("No Element with the given id exist");
-	var scrollInit = window.scrollY || window.pageYOffset;
+    var element = document.getElementById(elementid);    
+    if(!element){
+    	throw new Error("Unable to get sticker element by the id["+elementid+"]");
+    }
+    var hScroll = 0; 
+    var vScroll = 0; 
+    var oRect  =  element.getBoundingClientRect();    
+    var classnames = null;
+    var parent = null;
+    var oTop=0;
+    var isSticked =false;
+ //Create a custom array with contains and remove method   
+   var createCustomArray =(function(){
+		var array = new Array();		
+		array.contains =(function(classname){            
+             if(typeof classname !="string") return true;
+             for(var i=0; i < this.length ; i++){
+                 var temp=  this[i];
+                 if(temp == classname) return true;
+             }
+             return false;
+		}).bind(array);		
+		//Remove element from the array object
+        array.remove =(function(classname){
+        	if(typeof classname !="string")return false;
+          for(var i=0; i < this.length; i++){
+             if(this[i]==classname){
+             	return this.slice(i,1);
+             }
+          }return false;
+        }).bind(array)
+     return array;
+   });
 
-	this.initTop  = this.element.getBoundingClientRect().top + (scrollInit);   
-    //add class private method
-    var addClass=(function(classname){
-    	if(typeof classname !="string" || classname=="") return ;       	
-    	if(!this.element.classList.contains(classname))
-    		this.element.classList.add(classname);
-    }).bind(this);
- //remove class
- var removeClass =(function(classname){
- 	if(this.element.classList.contains(classname)){
- 		this.element.classList.remove(classname);
- 	}
- 	
- }).bind(this);
+/*Module private member function to add class the current selected element*/
+    var  addClass = (function(classname){
+      if(typeof classname === "string" && classname!==""){
+         if(!element.classList.contains(classname))
+         	element.classList.add(classname);
+      }
 
- var addSticky=(function()
- {
- 	if(!this.stickyclasses)this.stickyclasses=createArray();
- 	if(!this.unstickyclasses)this.unstickyclasses=createArray();
- 	this.stickyclasses.forEach(function(classname){ 					
- 		addClass(classname); 		
- 	});         	//add the unstick class now
- 	this.unstickyclasses.forEach(function(classname){
- 		removeClass(classname);
- 	});
+    });
 
- })
+ /*Module private member function remove class from the selected element if exists*/
+  var removeClass= (function(classname){
+        if(typeof classname=="string" && classname !=null){
+        	if(element.classList.contains(classname)){
+        		element.classList.remove(classname);
+        	}
+        }
+  });
+  
+/*Function memeber manage all the sticker classes to added to the class when stick callback is detected*/
+var stickElementClasses=(function(){
+      if(classnames){
+      	  classnames.forEach((function(classname){
+               addClass(classname);
+      	  }));
+      }
 
-
- var removeSticky=(function()
- {
- 	if(!this.unstickyclasses)this.unstickyclasses=createArray();
- 	if(!this.stickyclasses)this.stickyclasses=createArray();
- 	this.unstickyclasses.forEach(function(classname){
- 		addClass(classname);
-
- 	}) 			
- 	this.stickyclasses.forEach(function(classname){
- 		removeClass(classname);
- 	});
- })
- var onUpdate =(function()
- {
- 	yScroll = window.scrollY || window.pageYOffset ;
- 	var rect = this.element.getBoundingClientRect();
- 	this.scrollAmount = ( yScroll - this.initTop)
-
- 	if(yScroll >= 0){  	  	
- 		if(rect.top <=0){
- 			this.element.style.position="fixed";
- 			this.element.style.zIndex="1";
- 			this.sticked=true; 
- 			addSticky();
- 		}
- 	}
-    //check if finished scrolling
-    if(this.scrollAmount <= 0  && this.sticked==true){     
-    	this.element.style.position="";
-    	this.element.style.zIndex="";
-    	this.sticked=false;       
-    	removeSticky();
+  });
+var unStickElementClasses=(function(){
+    if(classnames){
+    	classnames.forEach((function(classname){
+           removeClass(classname);
+    	}));
     }
 
 });
- var createArray=(function()
- {
- 	var array = [];
- 	array.contains=(function(element)
- 	{
- 		for(var i=0; i < this.length; i++)
- 		{
- 			if(this[i]==element)return true;
+
+var addParentListener=(function(eventtype){
+
+     var isString =(typeof eventtype  =="string");
+	if( isString  && parent !=null){
+		 if(parent.addEventListener)
+		 	 parent.addEventListener(eventtype, onStickEvent, true);
+		 else if(parent.attachEvent)
+		 	parent.attachEvent("on"+eventtype, onStickEvent, true);		
+	}
+
+})
+
+var onStickEvent =(function(){  
+  window.requestAnimationFrame(onUpdate);
+  console.log("Loog")
+});
+
+var keepWidth=(function(abool){	
+   var elemParent = element.parentNode;
+   var pRect = (elemParent)?elemParent.getBoundingClientRect():null;
+   if(pRect){ 
+   	   var pWidth = (oRect.width * 100)/ pRect.width;
+   	   element.style.width=pWidth +"%";
+   	   
+   }
+});
+
+//update the current visibility position of the new window
+
+var onUpdate =(function(){
+	getScroll();
+ 	var rect = element.getBoundingClientRect();
+ 	var scrollAmount = ( vScroll - oTop)
+
+ 	if(vScroll >= 0){  	  	
+ 		if(rect.top <=0 && isSticked==false){
+ 			element.style.position="fixed"; 			
+ 		    element.style.zIndex="1";
+ 			isSticked = true; 
+ 			stickElementClasses();
+ 			keepWidth(true); 			
  		}
- 		return false;
-
- 	}).bind(array);
-
- 	array.remove=(function(classname)
- 	{
- 		for(var i=0; i< this.length ; i++){
- 			if(this[i]==classname){
- 				return this.slice(i,1);
- 			}
- 		}
- 	}).bind(array);
-
-
- 	return array;
-
- }).bind(this);
-
- var attachEvent=(function(element, eventtype, callback, aflag){
-
- 	if(element instanceof HTMLElement || element instanceof Window){
- 		element.addEventListener = element.addEventListener || element.attachEvent;
- 		if(element.addEventListener){
- 			if(typeof eventtype=="string")
- 				element.addEventListener(eventtype,callback,aflag);
-
- 		}
+ 		
  	}
- }).bind(this);
+    //check if finished scrolling
+    if(scrollAmount <= 0  && isSticked==true){     
+    	element.style.position="";
+    	element.style.zIndex="";
+    	isSticked=false;       
+    	unStickElementClasses();
+    	keepWidth(false);
+    }
+});
 
+var getScroll =(function(){
+	if(parent instanceof Window){
+             	vScroll =  parent.scrollY || parent.pageYOffset;
+             	hScroll  = parent.scrollX || parent.pageXOffset;             	
+             }else if(parent instanceof HTMLElement){
+             	vScroll =  parent.scrollTop  || 0;
+             	hScroll  = parent.scrollLeft || 0;
+             }
+})
+//*Private Initializer function /
+var onInit =(function(){
+	classnames = createCustomArray();
+})();
+  /*
+     The Expose part of the model
 
- var onScrollEvent=(function()
- {
+  **/
+   var ModuleStarted =false;
+   var InnerModule=({
+           getElement:(function(){
+            return element;
+           }),
+           start:(function(aparent){
+             if(!aparent)parent = window;
+              if(ModuleStarted==true)return ;           
+            
+              getScroll();
+             if(parent){
 
+                 addParentListener("scroll");
+                 addParentListener("resize");
+                 oTop   =  oRect.top + vScroll;
+                 ModuleStarted = true;                 
+                }
 
-    //update the information on the window when the window is ready to draw
-    window.requestAnimationFrame (onUpdate.bind(this));
-}).bind(this);
-//Initialised function that will added events listener to the window 
-var onInit =(function()
-{
-	if(window){	
-		attachEvent(window,"scroll",onScrollEvent.bind(this),true);
-		attachEvent(window,"resize",onScrollEvent.bind(this),true);
-	}	
-}).bind(this)();
-this.fn= {
-		 	//The function get the first element of the selection
-		 	first:(function()
-		 	{
-		 		if(this.isList) return this.elements[0];
-		 		return null;
-		 	}).bind(this),
-		 	/*
-             Interface to provided addition information when the onStickEvent is called
+           }), 
+           addCssClass:(function(classname){
+           	if(classnames){
+           		if(!classnames.contains(classname))
+           		  	 classnames.push(classname);           		
+           	}
+            return InnerModule;
+           }),         
+           clear:(function(){
+              unStickElement();
+              delete element;
+              delete oRect; 
+              delete classnames; 
+              ModuleStarted=false;   
+              return  InnerModule;   
+           })
+       
+    });
 
-             */
-             sticky:(function(){
-             	return {
-             		addClass:(function(classname){
-             			if(!this.stickyclasses) 
-             				this.stickyclasses= createArray();
-             			if(!this.stickyclasses.contains(classname))
-             				this.stickyclasses.push(classname);
-             			return this.fn;
-             		}).bind(this),
+   return InnerModule;
 
-             		removeClass:(function(classname)
-             		{
-             			if(!this.stickyclasses) 
-             				this.stickyclasses= createArray();
-             			if(this.stickyclasses.contains(classname)){
-             				this.stickyclasses.remove(classname);
-             			}
-             			return this.fn;
-             		}).bind(this)
-             	}
-             }).bind(this)()
-
-         }
-
-         return this.fn;
-
-     });
-
-
-
+});
